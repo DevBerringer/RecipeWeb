da4903d0d420c3b7c84d5b73549fbc2c75e68e58
devBerringer <blakeberringer@gmail.com>
2023-07-13 22:49:48 -0500

    added stuff

--- src/components/forms/NewRecipeForm3.tsx
+++ src/components/forms/NewRecipeForm3.tsx
@@ -1,12 +1,59 @@
 import { useState } from 'react';
 
 function NewRecipeForm2() {
-  const [steps, setSteps] = useState([]);
-  const [rating, setRating] = useState(0);
+  const [steps, setSteps] = useState(Array(3).fill(''));
+
+  const handleStepChange = (index, value) => {
+    const updatedSteps = [...steps];
+    updatedSteps[index] = value;
+    setSteps(updatedSteps);
+  };
+
+  const addStep = () => {
+    setSteps([...steps, '']);
+  };
+
+  const removeStep = (index) => {
+    const updatedSteps = [...steps];
+    updatedSteps.splice(index, 1);
+    setSteps(updatedSteps);
+  };
 
   return (
     <div className="max-w-md mx-auto p-4 bg-recipecentral shadow-lg rounded">
       <h2 className="text-2xl font-semibold mb-4">Steps:</h2>
+      {steps.map((step, index) => (
+        <div className="mb-4" key={index}>
+          <label
+            htmlFor={`description-${index}`}
+            className="block mb-1 font-semibold"
+          >
+            Step {index + 1}
+          </label>
+          <textarea
+            id={`description-${index}`}
+            value={step}
+            onChange={(e) => handleStepChange(index, e.target.value)}
+            placeholder="Enter step"
+            className="w-full px-4 py-2 border border-gray-300 rounded"
+            rows="4"
+          />
+          <button
+            type="button"
+            onClick={() => removeStep(index)}
+            className="text-red-500 font-semibold mt-2"
+          >
+            Remove Step
+          </button>
+        </div>
+      ))}
+      <button
+        type="button"
+        onClick={addStep}
+        className="bg-blue-500 text-white font-semibold py-2 px-4 rounded"
+      >
+        Add Step
+      </button>
     </div>
   );
 }
--- src/components/pages/Recipes.tsx
+++ src/components/pages/Recipes.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getRecipes } from '../../api/api';
 import RecipeCard from '../shared/RecipeCard';
+import SearchBar from '../shared/SearchBar';
 
 const DEFAULT = {
   Message: '',
@@ -10,15 +11,18 @@ const DEFAULT = {
 };
 
 function Recipes() {
-  const [data, setData] = useState<RecipeResponseDto>(DEFAULT);
+  const [data, setData] = useState(DEFAULT);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const getData = async (): Promise<void> => {
+    const getData = async () => {
       try {
-        const fetchedData: RecipeResponseDto = await getRecipes();
+        const fetchedData = await getRecipes();
         setData(fetchedData);
       } catch (error) {
         // Handle error, e.g., show an error message or retry
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -27,15 +31,35 @@ function Recipes() {
 
   return (
     <div>
-      {data.RecipeDTOs.map((item) => (
-        <Link to={`${item.Id}`} key={item.Id}>
-          <RecipeCard
-            name={item.Name}
-            description={item.Description}
-            cookTime={item.CookTimeMin.toLocaleString()}
-          />
-        </Link>
-      ))}
+      <div className="relative flex mx-auto mb-4">
+        <div className="px-4 py-2 text-gray-600 text-lg">
+          Can't find what you are looking for?
+        </div>
+        <SearchBar />
+      </div>
+      {loading ? (
+        <div className="h-screen">Loading...</div>
+      ) : (
+        <div>
+          {data.RecipeDTOs.length > 0 ? (
+            data.RecipeDTOs.map((item) => (
+              <Link to={`${item.Id}`} key={item.Id}>
+                <RecipeCard
+                  name={item.Name}
+                  description={item.Description}
+                  cookTime={item.CookTimeMin.toLocaleString()}
+                />
+              </Link>
+            ))
+          ) : (
+            <div className="flex items-center justify-center my-4">
+              <div className="max-w-lg bg-gray-100 border border-gray-300 rounded p-4 text-gray-500 text-lg text-center">
+                No recipes found
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }

56eab1829aedd62d6e868be517c7f7c21bdd65a8
devBerringer <blakeberringer@gmail.com>
2025-07-01 16:09:37 -0500

    fixes

--- src/components/pages/navbar/ProfileNav.tsx
+++ src/components/pages/navbar/ProfileNav.tsx
@@ -3,7 +3,7 @@ import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { useNavigate } from 'react-router-dom';
 
-import { ReactComponent as ProfileIcon } from '../../../assets/ProfileIcon.svg';
+import { ReactComponent as ProfileIcon } from '../../../assets/profileicon.svg';
 import { ReactComponent as GroupIcon } from '../../../assets/groupicon.svg';
 import { ReactComponent as LogoutIcon } from '../../../assets/logoutIcon.svg';
 
